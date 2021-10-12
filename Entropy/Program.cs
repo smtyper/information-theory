194eac3159a425c5ec94ff066ceb390d771dea78
Artem <smtyper.contact@gmail.com>
2021-10-13 02:08:21 +0300

    Calculate book entropy

--- Entropy/Program.cs
+++ Entropy/Program.cs
@@ -1,1 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
+var bookText = await File.ReadAllTextAsync("source.txt");
+var textEntropy = bookText
+    .GroupBy(chr => chr)
+    .Select(group => group.Count() / (double)bookText.Length)
+    .Sum(frequency => frequency * Math.Log2(1 / frequency));
+
+Console.WriteLine(textEntropy);
