98c116a6c3bb1af36b8a5721f4c7fc13b622b0bc
Artem <smtyper.contact@gmail.com>
2021-10-13 16:33:12 +0300

    Get image entropy

--- Entropy/Program.cs
+++ Entropy/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
 
 var bookText = await File.ReadAllTextAsync("source.txt");
-var textEntropy = bookText
-    .GroupBy(chr => chr)
-    .Select(group => group.Count() / (double)bookText.Length)
-    .Sum(frequency => frequency * Math.Log2(1 / frequency));
+var textEntropy = GetEntropy(bookText.ToCharArray());
+var textInformation = bookText.Length * textEntropy;
 
-Console.WriteLine(textEntropy);
+await using var imageStream = File.OpenRead("source.jpg");
+using var image = await Image.LoadAsync<Rgba32>(Configuration.Default, imageStream);
+var pixels = image.GetPixelMemoryGroup()
+    .Single()
+    .ToArray();
+var imageEntropy = GetEntropy(pixels);
+var imageInformation = pixels.Length * imageEntropy;
+
+Console.WriteLine();
+
+static double GetEntropy<T>(IReadOnlyCollection<T> elements) => elements
+    .GroupBy(element => element)
+    .Select(group => group.Count() / (double)elements.Count)
+    .Sum(frequency => frequency * Math.Log2(1 / frequency));
