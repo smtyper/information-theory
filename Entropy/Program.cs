7e71cac27fc5c2d01dde1a269bb41be90a70e6d0
Artem <smtyper.contact@gmail.com>
2021-10-14 02:17:43 +0300

    Convert source image to black and white

--- Entropy/Program.cs
+++ Entropy/Program.cs
@@ -5,22 +5,29 @@ using System.Linq;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 var bookText = await File.ReadAllTextAsync("source.txt");
 var textEntropy = GetEntropy(bookText.ToCharArray());
 var textInformation = bookText.Length * textEntropy;
 
-await using var imageStream = File.OpenRead("source.jpg");
-using var image = await Image.LoadAsync<Rgba32>(Configuration.Default, imageStream);
-var pixels = image.GetPixelMemoryGroup()
-    .Single()
-    .ToArray();
+await using var imageFileStream = File.OpenRead("source.jpg");
+
+using var sourceImage = await Image.LoadAsync<L8>(Configuration.Default, imageFileStream);
+using var blackWhiteImage = sourceImage.Clone(context => context.BlackWhite());
+
+var pixels = GetImagePixels(blackWhiteImage);
 var imageEntropy = GetEntropy(pixels);
-var imageInformation = pixels.Length * imageEntropy;
+var imageInformation = pixels.Count * imageEntropy;
 
 Console.WriteLine();
 
-static double GetEntropy<T>(IReadOnlyCollection<T> elements) => elements
+static IReadOnlyCollection<TPixel> GetImagePixels<TPixel>(Image<TPixel> image)
+    where TPixel : unmanaged, IPixel<TPixel> => image.GetPixelMemoryGroup()
+    .Single()
+    .ToArray();
+
+static double GetEntropy<TElement>(IReadOnlyCollection<TElement> elements) => elements
     .GroupBy(element => element)
     .Select(group => group.Count() / (double)elements.Count)
     .Sum(frequency => frequency * Math.Log2(1 / frequency));
