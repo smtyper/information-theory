456dd38765ad35428b959070a87c471e996e336f
Artem <smtyper.contact@gmail.com>
2021-10-15 02:10:36 +0300

    Save results to file

--- Entropy/Program.cs
+++ Entropy/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Advanced;
@@ -11,49 +12,46 @@ using SixLabors.ImageSharp.Processing;
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
-var textPath = configuration["TextPath"];
-var imagePath = configuration["ImagePath"];
 
+var textFile = configuration["TextFile"];
+var bookText = await File.ReadAllTextAsync(textFile);
+var characherArray = bookText.ToCharArray();
 
-var bookText = await File.ReadAllTextAsync(textPath);
-var textEntropy = GetEntropy(bookText.ToCharArray());
-var textInformation = bookText.Length * textEntropy;
+var (textEntropy, textInformation) = await GetInformationAsync(characherArray);
 
-await using var imageFileStream = File.OpenRead(imagePath);
-
+var imageFile = configuration["ImageFile"];
+await using var imageFileStream = File.OpenRead(imageFile);
 using var sourceImage = await Image.LoadAsync<L8>(Configuration.Default, imageFileStream);
 using var grayscaleImage = sourceImage.Clone(context => context.Grayscale());
+await grayscaleImage.SaveAsJpegAsync("grayscale.jpg");
+var intensities = GetImagePixels(grayscaleImage)
+    .Select(pixel => pixel.PackedValue)
+    .ToArray();
 
-var pixels = GetImagePixels(grayscaleImage);
-var imageEntropy = GetEntropy(pixels);
-var imageInformation = pixels.Count * imageEntropy;
+var (imageEntropy, imageInformation) = await GetInformationAsync(intensities);
 
-Console.WriteLine($"{nameof(textEntropy)}: {textEntropy}\t{nameof(imageEntropy)}: {imageEntropy}");
-Console.WriteLine($"{nameof(textInformation)}: {textInformation}\t{nameof(imageInformation)}: {imageInformation}");
-
-foreach (var compression in new[] { 2, 4, 8 })
-{
-    using var compressedImage = grayscaleImage.Clone(context =>
-        context.Pad(grayscaleImage.Width / compression, grayscaleImage.Height / compression));
-    var compressedImagePixels = GetImagePixels(compressedImage);
-
-    var compressedImageEntropy = GetEntropy(compressedImagePixels);
-    var compressedImageInformation = compressedImageEntropy * compressedImagePixels.Count;
-
-    Console.Write($"compression: 1 / {compression}\t");
-    Console.Write($"entropy:{compressedImageEntropy}\t");
-    Console.Write($"information: {compressedImageInformation}\t");
-    Console.Write($"comparison: {imageInformation / compressedImageInformation}\n");
-}
-
-Console.WriteLine();
 
 static IReadOnlyCollection<TPixel> GetImagePixels<TPixel>(Image<TPixel> image)
     where TPixel : unmanaged, IPixel<TPixel> => image.GetPixelMemoryGroup()
     .Single()
     .ToArray();
 
-static double GetEntropy<TElement>(IReadOnlyCollection<TElement> elements) => elements
-    .GroupBy(element => element)
-    .Select(group => group.Count() / (double)elements.Count)
-    .Sum(frequency => frequency * Math.Log2(1 / frequency));
+static async Task<(double Entropy, double Information)> GetInformationAsync<TElement>(
+    IReadOnlyCollection<TElement> elements, string savePath = null)
+{
+    var elementGroups = elements
+        .GroupBy(element => element)
+        .Select(group => (group.Key, Count: group.Count(), Chance: group.Count() / (double)elements.Count))
+        .ToArray();
+    var entropy = elementGroups.Sum(group => group.Chance * Math.Log2(1 / group.Chance));
+    var information = entropy * elements.Count;
+
+    if (savePath is null)
+        return (entropy, information);
+
+    File.Delete(savePath);
+    await File.AppendAllLinesAsync(savePath, elementGroups
+        .Select(elementGroup => string.Join('\t', elementGroup.Key, elementGroup.Count, elementGroup.Chance))
+        .Append($"entropy: {entropy}\tinformation: {information}"));
+    return (entropy, information);
+}
