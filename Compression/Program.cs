48b51947bb2fc449be2cb8e10115af5b72d37d5a
Artem <smtyper.contact@gmail.com>
2021-11-07 02:55:23 +0300

    Implement text file compression

--- Compression/Program.cs
+++ Compression/Program.cs
@@ -1,1 +1,16 @@
-﻿using System;
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+var sourceFilePath = configuration["TextFile"];
+
+await using (var sourceFileStream = File.OpenRead(sourceFilePath))
+{
+    await using var writeStream = File.OpenWrite($"{sourceFilePath}.gz");
+    await using var compressionStream = new GZipStream(writeStream, CompressionMode.Compress);
+
+    await sourceFileStream.CopyToAsync(compressionStream);
+}
