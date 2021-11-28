0ef90d9d471642c5b64d269b21507ac6b97d2650
Artem <smtyper.contact@gmail.com>
2021-11-28 13:34:12 +0300

    Read database content

--- GradientDescent/Program.cs
+++ GradientDescent/Program.cs
@@ -1,7 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 var dbFilePath = configuration["DbFile"];
+var vectors = (await File.ReadAllLinesAsync(dbFilePath))
+    .Skip(2)
+    .Select(line =>
+    {
+        var splittedLine = line.Split('\t');
+
+        var y = double.Parse(splittedLine[0], CultureInfo.InvariantCulture);
+        var vector = splittedLine[1..]
+            .Select(numberString => double.Parse(numberString, CultureInfo.InvariantCulture))
+            .Prepend(1)
+            .ToArray();
+
+        return (y, vector);
+    })
+    .ToArray();
