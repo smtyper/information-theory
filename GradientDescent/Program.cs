75da056ccc0d89c1fad1d1bed9b25d045935c22f
Artem <smtyper.contact@gmail.com>
2021-11-28 10:40:22 +0300

    Add database file

--- GradientDescent/Program.cs
+++ GradientDescent/Program.cs
@@ -4,3 +4,4 @@ using Microsoft.Extensions.Configuration;
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
+var dbFilePath = configuration["DbFile"];
